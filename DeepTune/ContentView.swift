f7a5fbed3918876ee2d8be15650563758c38d62d
Ali Deniz Yakar <denizyakar00@gmail.com>
2026-03-16 20:38:11 +0300

    Initial Commit

---
+++ DeepTune/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeepTune
+//
+//  Created by Ali Deniz Yakar on 16.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeepTune/DeepTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepTuneApp.swift
+//  DeepTune
+//
+//  Created by Ali Deniz Yakar on 16.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct DeepTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
